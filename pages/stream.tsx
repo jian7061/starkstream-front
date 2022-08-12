05603e2248fbeca41c70bf7e4a45ab4c32ebf3e5
Jian Kim <jian7061@gmail.com>
2022-08-13 01:28:47 +0900

    add WrapContainer

--- pages/stream.tsx
+++ pages/stream.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
+import styled from "styled-components";
 
 const Send: NextPage = () => {
-  return <div>stream</div>;
+  return (
+    <Wrapper>
+      <MainContainer>Stream</MainContainer>
+    </Wrapper>
+  );
 };
 
 export default Send;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 5rem;
+`;
+
+const MainContainer = styled.div``;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -78,7 +78,7 @@ const Wrapper = styled.div`
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 3rem;
+  margin-top: 5rem;
 `;
 
 const Button = styled.button`
