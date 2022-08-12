67a4a4bb032faac91953a05f438045ba78ac8c60
Jian Kim <jian7061@gmail.com>
2022-08-12 20:29:39 +0900

    Add an example modal

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,45 @@
 import styled from "styled-components";
+import Modal from "react-modal";
+import { useRouter } from "next/router";
+Modal.setAppElement("#__next");
+import { useState } from "react";
+import NewBuildModal from "../components/modal/NewBuildModal";
 
 export default function Main() {
+  const [isOpen, setIsOpen] = useState(false);
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "white",
+      padding: "1rem",
+      border: "none",
+    },
+    overlay: {
+      backgroundColor: "rgba(168, 180, 202, 0.75)",
+    },
+  };
   return (
     <Wrapper>
-      <MainContainer>dashboard</MainContainer>
+      <MainContainer>
+        <Button
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          Click to show modal
+        </Button>
+        <Modal
+          isOpen={isOpen}
+          onRequestClose={() => setIsOpen(false)}
+          style={customStyles}
+        >
+          <NewBuildModal setIsOpen={setIsOpen} />
+        </Modal>
+      </MainContainer>
     </Wrapper>
   );
 }
@@ -11,3 +47,9 @@ export default function Main() {
 const Wrapper = styled.div``;
 
 const MainContainer = styled.div``;
+
+const Button = styled.button`
+  &:hover {
+    cursor: pointer;
+  }
+`;
